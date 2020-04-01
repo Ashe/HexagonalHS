c36097209fc850b74c15243a33c7fe1b52a08fc8
Ashley Smith <ashley@aas.sh>
2020-04-01 18:35:17 +0100

    Initial commit

---
+++ src/Main.hs
@@ -0,0 +1,53 @@
+module Main where
+
+import qualified Graphics.UI.GLFW as GLFW
+import Graphics.Rendering.OpenGL (($=))
+import qualified Graphics.Rendering.OpenGL as GL
+import Control.Monad (when)
+import Control.Concurrent (threadDelay)
+import System.Environment (getArgs, getProgName)
+
+-- Entry point
+main :: IO ()
+main = do
+
+  -- Start main loop if GLFW initialised succeessfully
+  r <- GLFW.init
+  when r $ do
+
+    -- Set error callback in case initialisation fails
+    GLFW.setErrorCallback $ Just simpleErrorCallback
+
+    -- Open window
+    m <- GLFW.createWindow 400 400 "GLFW Demo" Nothing Nothing
+    case m of
+      (Just win) -> do
+
+        -- Set the color to clear background
+        GL.clearColor $= GL.Color4 0 0 0 0
+
+        -- Set callbacks
+        GLFW.setWindowSizeCallback win $ Just windowSizeCallback
+
+        -- @TODO: Do main loop here
+        threadDelay 2000000
+
+        -- Destroy window
+        GLFW.destroyWindow win
+
+      -- If the window wasn't created, do nothing
+      Nothing -> pure()
+
+    -- Terminate GLFW and close program
+    GLFW.terminate
+
+  where
+    simpleErrorCallback :: GLFW.ErrorCallback
+    simpleErrorCallback e s = putStrLn $ unwords [show e, show s]
+    windowSizeCallback :: GLFW.WindowSizeCallback
+    windowSizeCallback win w h = do
+      let size = GL.Size (fromIntegral w) (fromIntegral h)
+      GL.viewport   $= (GL.Position 0 0, size)
+      GL.matrixMode $= GL.Projection
+      GL.loadIdentity
+      GL.ortho2D 0 (realToFrac w) (realToFrac h) 0
